a00b598d526fe264d215667676e31857be0fe2dd
GiridharSalana <GiridharSalana@gmail.com>
2020-08-05 19:02:12 +0530

    SubstractNumbers.py Created

---
+++ SubNumbers.py
@@ -0,0 +1,9 @@
+#This program substracts two numbers
+#Taking First Number from stdin
+num1 = input("Enter First Number?\n")
+#Taking Second Numer from stdin
+num2 = input("Enter Second Number?\n")
+#Addition
+sumall = float(num1)-float(num2) 
+#Output Sum to stdout
+print('The difference of {0} and {1} is {2}'.format(num1,num2,sumall))i
