e66bf761649983b39aa9465528b5a33b7ead027b
GiridharSalana <GiridharSalana@gmail.com>
2020-08-05 18:58:08 +0530

    AddNumbers.py Script Created

---
+++ AddNumbers.py
@@ -0,0 +1,9 @@
+#This program adds two numbers
+#Taking First Number from stdin
+num1 = input("Enter First Number?\n")
+#Taking Second Numer from stdin
+num2 = input("Enter Second Number?\n")
+#Addition
+sumall = float(num1)+float(num2) 
+#Output Sum to stdout
+print('The sum of {0} and {1} is {2}'.format(num1,num2,sumall))
