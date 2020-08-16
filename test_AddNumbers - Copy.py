d9da798abf3e62e193026c6388596cd2971486aa
GiridharSalana <GiridharSalana@gmail.com>
2020-08-16 18:27:16 +0530

    testing with ci

---
+++ test_AddNumbers - Copy.py
@@ -0,0 +1,9 @@
+#This program adds two numbers
+#Taking First Number from stdin
+num1 = 5
+#Taking Second Numer from stdin
+num2 = 10000
+#Addition
+sumall = float(num1)+float(num2) 
+#Output Sum to stdout
+print('The sum of {0} and {1} is {2}'.format(num1,num2,sumall))
